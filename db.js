147d2bbd33903a4cee118d51b35e7f269098a103
krissy <kmujakic@gmail.com>
2023-10-02 18:35:01 +0200

    Database connection for messagely

--- db.js
+++ db.js
@@ -1,12 +1,18 @@
 /** Database connection for messagely. */
 
-
 const { Client } = require("pg");
 const { DB_URI } = require("./config");
 
 const client = new Client(DB_URI);
 
-client.connect();
 
+client.connect()
+    .then(() => {
+        console.log('Connected to the PostgreSQL database');
+    })
+    .catch((error) => {
+        //  throw new Error('SASL: SCRAM-SERVER-FIRST-MESSAGE: client password must be a string')
+        console.error('Error connecting to the database:', error.message);
+    });
 
 module.exports = client;
